ba454b034237e271574534fb9009b4e2109c0757
Pakorn Jitngamplang <pakorn@win.local>
2020-01-04 16:44:03 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    componentDidMount() {
+        const array = document.querySelectorAll(".word");
+        var i = 0;
+        setInterval(() => {
+            array[i % 5].classList.add("active");
+            i += 1;
+        }, 1500);
+    }
+
+    end(e) {
+        e.target.classList.remove("active");
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <div className="ctn">
+                    <div className="word" onAnimationEnd={e => this.end(e)}>
+                        hello
+                    </div>
+                    <div
+                        className="word"
+                        onAnimationEnd={e => this.end(e)}
+                        style={{ lineHeight: "115%" }}
+                    >
+                        สวัสดี
+                    </div>
+                    <div className="word" onAnimationEnd={e => this.end(e)}>
+                        hola
+                    </div>
+                    <div
+                        className="word"
+                        onAnimationEnd={e => this.end(e)}
+                        style={{ lineHeight: "115%" }}
+                    >
+                        你好
+                    </div>
+                    <div
+                        className="word"
+                        onAnimationEnd={e => this.end(e)}
+                        style={{ lineHeight: "115%" }}
+                    >
+                        안녕
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
